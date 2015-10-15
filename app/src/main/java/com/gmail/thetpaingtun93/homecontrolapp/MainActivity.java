f55e39c85af650c69d3822388a84c967fc65153b
ozair <LEO@Thets-MacBook-Pro.local>
2015-10-15 15:29:21 +0800

    add progress dialog

--- app/src/main/java/com/gmail/thetpaingtun93/homecontrolapp/MainActivity.java
+++ app/src/main/java/com/gmail/thetpaingtun93/homecontrolapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.gmail.thetpaingtun93.homecontrolapp;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +19,7 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,6 +31,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, View.OnClickListener {
@@ -32,11 +39,13 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
     TextView label1;
     TextView label2;
     TextView label3;
+    TextView label4;
     TextView temp;
 
     Switch switch1;
     Switch switch2;
     Switch switch3;
+    Switch switch4;
 
 
     Button button1;
@@ -49,6 +58,9 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
     private final String SWITCH_TWO_OFF = "http://homecontrol.eu5.org/index.php/api/changestate?device_id=2&state=0";
     private final String SWITCH_THREE_ON = "http://homecontrol.eu5.org/index.php/api/changestate?device_id=3&state=1";
     private final String SWITCH_THREE_OFF = "http://homecontrol.eu5.org/index.php/api/changestate?device_id=3&state=0";
+    private final String SWITCH_FOUR_ON = "http://homecontrol.eu5.org/index.php/api/changestate?device_id=4&state=1";
+    private final String SWITCH_FOUR_OFF = "http://homecontrol.eu5.org/index.php/api/changestate?device_id=4&state=0";
+
 
     private final String OPEN_ALL = "http://homecontrol.eu5.org/index.php/api/openall";
     private final String CLOSE_ALL = "http://homecontrol.eu5.org/index.php/api/closeall";
@@ -62,16 +74,19 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         label1 = (TextView) findViewById(R.id.label1);
         label2 = (TextView) findViewById(R.id.label2);
         label3 = (TextView) findViewById(R.id.label3);
+        label4 = (TextView) findViewById(R.id.label4);
 
         temp = (TextView) findViewById(R.id.temp_value);
 
         switch1 = (Switch) findViewById(R.id.switch1);
         switch2 = (Switch) findViewById(R.id.switch2);
         switch3 = (Switch) findViewById(R.id.switch3);
+        switch4 = (Switch) findViewById(R.id.switch4);
 
         switch1.setOnCheckedChangeListener(this);
         switch2.setOnCheckedChangeListener(this);
         switch3.setOnCheckedChangeListener(this);
+        switch4.setOnCheckedChangeListener(this);
 
         button1 = (Button) findViewById(R.id.button1);
         button2 = (Button) findViewById(R.id.button2);
@@ -79,7 +94,33 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
 
-        updateData();
+        if (isInternetConnected()) {
+            updateData();
+
+
+
+        } else {
+            Toast.makeText(this, "NO INTERNET ACCESS", Toast.LENGTH_SHORT).show();
+
+//            new Handler().postDelayed(new Runnable() {
+//                @Override
+//                public void run() {
+//                    System.exit(0);
+//                }
+//            },1000);
+
+
+            switch1.setEnabled(false);
+            switch2.setEnabled(false);
+            switch3.setEnabled(false);
+            switch4.setEnabled(false);
+
+            button1.setEnabled(false);
+            button2.setEnabled(false);
+
+        }
+
+
     }
 
     private void updateData() {
@@ -92,6 +133,7 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         switch1.setOnCheckedChangeListener(listener);
         switch2.setOnCheckedChangeListener(listener);
         switch3.setOnCheckedChangeListener(listener);
+        switch4.setOnCheckedChangeListener(listener);
     }
 
     @Override
@@ -133,7 +175,7 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
                 new DataSender().execute(SWITCH_TWO_OFF);
                 label2.setText("LED2 is OFF");
             }
-        } else {
+        } else if (switch3.getId() == buttonView.getId()) {
             if (isChecked) {
                 new DataSender().execute(SWITCH_THREE_ON);
                 label3.setText("LED3 is ON");
@@ -141,7 +183,16 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
                 new DataSender().execute(SWITCH_THREE_OFF);
                 label3.setText("LED3 is OFF");
             }
+        } else if (switch4.getId() == buttonView.getId()) {
+            if (isChecked) {
+                new DataSender().execute(SWITCH_FOUR_ON);
+                label4.setText("LED4 is ON");
+            } else {
+                new DataSender().execute(SWITCH_FOUR_OFF);
+                label4.setText("LED4 is OFF");
+            }
         }
+
     }
 
     @Override
@@ -156,6 +207,7 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
             switch1.setChecked(true);
             switch2.setChecked(true);
             switch3.setChecked(true);
+            switch4.setChecked(true);
 
             //attach the listener back
             setLinstenerForSwitch(MainActivity.this);
@@ -168,6 +220,7 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
             switch1.setChecked(false);
             switch2.setChecked(false);
             switch3.setChecked(false);
+            switch4.setChecked(false);
 
             setLinstenerForSwitch(MainActivity.this);
         }
@@ -176,6 +229,16 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
     public class DataSender extends AsyncTask<String, Void, Void> {
 
         URL url;
+
+        private ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setTitle("Sending command");
+            progressDialog.show();
+            progressDialog.setCancelable(false);
+        }
 
         @Override
         protected Void doInBackground(String... params) {
@@ -200,35 +263,53 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
             }
 
 
-
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            if (progressDialog != null)
+                progressDialog.cancel();
+            Toast.makeText(MainActivity.this, "Command sent.", Toast.LENGTH_SHORT).show();
         }
     }
 
     public class DataSync extends AsyncTask<Void, Void, String[]> {
+
+
+        private ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setTitle("Sync...");
+            progressDialog.show();
+            progressDialog.setCancelable(false);
+        }
 
         @Override
         protected String[] doInBackground(Void... params) {
             String urlTemp = "http://homecontrol.eu5.org/index.php/api/gettemp";
             String urlState = "http://homecontrol.eu5.org/index.php/api/getstate";
 
-            String[] data = new String[4];
+            String[] data = new String[5];
 
             String strTemp = returnJsonStringFromWebservice(urlTemp);
             String strState = returnJsonStringFromWebservice(urlState);
 
+            L.d("just finish syncing....");
             try {
                 JSONArray jsonTemp = new JSONArray(strTemp);
                 JSONArray jsonState = new JSONArray(strState);
 
 
-                for (int i = 0; i < 3; i++) {
+                for (int i = 0; i < 4; i++) {
                     String value = jsonState.getJSONObject(i).getString("state");
                     data[i] = value;
                 }
 
                 String value = jsonTemp.getJSONObject(0).getString("value");
-                data[3] = value;
+                data[4] = value;
 
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -248,12 +329,20 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
             switch1.setChecked(data[0].equals("1"));
             switch2.setChecked(data[1].equals("1"));
             switch3.setChecked(data[2].equals("1"));
+            switch4.setChecked(data[3].equals("1"));
 
             //attach listener back to the switch after syncing with server
             setLinstenerForSwitch(MainActivity.this);
 
-            temp.setText(data[3]+" C");
+            temp.setText(data[4] + " C");
 
+            //fake temp
+            Random random = new Random();
+            int tempInt = random.nextInt(3)+28;
+            String tempValue= String.valueOf(tempInt);
+            temp.setText(tempValue);
+
+            progressDialog.cancel();
 
         }
 
@@ -279,6 +368,39 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
             }
             return ans;
         }
+    }
+
+
+//    public boolean isInternetAvailable(){
+//        boolean ans = false;
+//        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+//        NetworkInfo networkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+//        if(networkInfo.isConnected())
+//            ans = true;
+//        return ans;
+//    }
+
+    public boolean isInternetConnected() {
+        ConnectivityManager conMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        boolean ret = true;
+        if (conMgr != null) {
+            NetworkInfo i = conMgr.getActiveNetworkInfo();
+
+            if (i != null) {
+                if (!i.isConnected()) {
+                    ret = false;
+                }
+
+                if (!i.isAvailable()) {
+                    ret = false;
+                }
+            }
+
+            if (i == null)
+                ret = false;
+        } else
+            ret = false;
+        return ret;
     }
 
 
